841686567695d77f67c07181ad7a420e0f2df4c5
NaveenkumarRamesh <70542093+NaveenkumarRamesh@users.noreply.github.com>
2020-11-24 17:35:13 +0530

    Test2 (#1)
    * commited
    
    * Update __init__.py
    
    Co-authored-by: Naveen Kumar Ramesh <naveenkumar.ramesh@ad.infosys.com>

--- basicapp/testing/manage.py
+++ basicapp/testing/manage.py
@@ -4,8 +4,19 @@ import os
 import sys
 
 
+
+
+
+
+
+
+
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testing.settings')
+    os.environ.setdefault
+
+
+
+('DJANGO_SETTINGS_MODULE', 'testing.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
